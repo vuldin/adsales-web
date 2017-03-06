09f60447d09a150b93d8a6635c222a093c428eb3
Joshua Purcell <jlpurcell@us.ibm.com>
2017-03-06 13:56:03 -0800

    cleanup dumb initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import WebFontLoader from 'webfontloader'
+import './App.scss'
+import { observer } from 'mobx-react'
+import { Card, CardTitle, CardText, CardActions } from 'react-md/lib/Cards'
+import Button from 'react-md/lib/Buttons'
 
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+})
+
+@observer
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let store = this.props.store
+    let undo = this.props.undo
+    return <div className='md-grid'>
+      <Card className='md-cell'>
+        <CardTitle title={store.welcomeMessage}/>
+        <CardText>
+          <span>{store.counter}</span>
+        </CardText>
+        <CardActions>
+          <Button flat label='Increase' onClick={store.incCounter}/>
+          <Button flat label='Undo' disabled={!undo.canUndo} onClick={undo.undo}/>
+          <Button flat label='Redo' disabled={!undo.canRedo} onClick={undo.redo}/>
+        </CardActions>
+      </Card>
+    </div>
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Store from './store'
+import { Undo } from 'json-mobx'
 
-const store = createStore(reducer)
+const store = new Store()
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+const undo = new Undo(store)
+/*
+const undo = new Undo(store, after => {
+  console.log('undo')
+  console.log(JSON.stringify(after, null, 2))
+})
+*/
+
+ReactDOM.render(
+  <App store={store} undo={undo}/>,
   document.getElementById('root')
 )
